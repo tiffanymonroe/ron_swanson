b0e798669dd55ddce8fa2ecf803c74e91799314f
Tiffany Monroe <hello@tiffanymonroe.com>
2018-02-28 20:57:35 -0800

    connected to api

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-console.log("I'm Ron F***ing Swanson");
-
 const app = angular.module('Ron', []);
 
 app.controller('MainController', ['$http', function($http){
   const controller = this;
-  
+
+
+  //function to request one quote from API
+  this.getQuotes = function(){
+    $http({
+      method: 'get',
+      url: 'http://ron-swanson-quotes.herokuapp.com/v2/quotes'
+    }).then(
+      function(res){
+        controller.quotes = res.data[0];
+        console.log(controller.quotes);
+      },
+      function(err){
+        console.log("I'm Ron F***ing Swanson");
+      }
+    )
+  }
 
 }]); //end of MainController
