2d779abeb102327058a2b755bb8c936db75112ce
Tiffany Monroe <hello@tiffanymonroe.com>
2018-02-28 19:51:00 -0800

    file setup

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("I'm Ron F***ing Swanson");
+
+const app = angular.module('Ron', []);
+
+app.controller('MainController', ['$http', function($http){
+  const controller = this;
+  
+
+}]); //end of MainController
